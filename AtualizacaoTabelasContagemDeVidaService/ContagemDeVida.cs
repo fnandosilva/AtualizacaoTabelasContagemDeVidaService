19c4afd521c1ac215ca0d7e750f5be3e1c5d389d
Fernando Silva <fnando2-silva@yahoo.com.br>
2020-08-19 11:56:45 -0300

    Add project files.

---
+++ AtualizacaoTabelasContagemDeVidaService/ContagemDeVida.cs
@@ -0,0 +1,66 @@
+﻿using Mantris.Acts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AtualizacaoTabelasContagemDeVidaService
+{
+
+    public class ContagemDeVida
+    {
+        private readonly System.Timers.Timer _timer;
+        public static int dia = 26;
+        public static int mes = DateTime.Now.Month;
+        public static int ano = DateTime.Now.Year;
+        public static int hora = DateTime.Now.Hour;
+        public static int minuto = DateTime.Now.Minute;
+        public static int segundo = DateTime.Now.Second;
+
+        public DateTime dataToBeExecuted = new DateTime(ano, mes, dia, hora, minuto, segundo);
+
+        public ContagemDeVida()
+        {
+            bool IsTheDateTime = (dataToBeExecuted == DateTime.Now);
+            
+            //_timer = new System.Timers.Timer(2592000) { AutoReset = true };
+            _timer = new System.Timers.Timer(86400) { AutoReset = true };
+
+            if (IsTheDateTime == true)
+            {
+                _timer.Elapsed += Timer_Elapsed;
+            }
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Carregando a tabela Empresa_SocNet.");
+            //var listaEmpresaTabela = ContagemDeVidaEmpresaSocNet_.Carregar_Colecao("true");
+
+            Console.WriteLine("Atualizando a tabela Funcionario_Socnet.");
+            //ContagemDeVidaFuncionarioSocNet_.Gravar_ColecaoFuncionarios_Exportadados(listaEmpresaTabela, true);
+
+            Console.WriteLine("Atualizando campos que foram adiconados a tabela Funcionario_Socnet.");
+            //ContagemDeVidaFuncionarioSocNet_.Atualizar_TodosCampos_TabelaFuncionarios(listaEmpresaTabela);
+
+            Console.WriteLine("Atualizando a tabela Funcionario_Socnet.");
+            //ContagemDeVidaFuncionarioMantris_.Gravar_ColecaoFuncionarios_Exportadados(listaEmpresaTabela, mes, ano, true);
+
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ AtualizacaoTabelasContagemDeVidaService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace AtualizacaoTabelasContagemDeVidaService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<ContagemDeVida>(s =>
+                {
+                    s.ConstructUsing(contagemVida => new ContagemDeVida());
+                    s.WhenStarted(contagemVida => contagemVida.Start());
+                    s.WhenStopped(contagemVida => contagemVida.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("AtualizacaoTabelasContagemDeVidaService");
+                x.SetDisplayName("Atualizacao Tabelas Contagem De Vida Service");
+                x.SetDescription("Este serviço atualiza tabelas usadas pelo outro serviço de Contagem de Vidas no Importer.");
+
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
